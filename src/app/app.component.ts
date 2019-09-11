e3e918b83ac5f0794f2c459d83135f2c161ee22a
Aitor Espinosa <aitor@Mac-mini-de-Aitor.local>
2019-09-11 11:46:22 +0200

    App listen to city$ subscriber

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CityService } from './city.service';
+import { City } from './city';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'wetter-proj';
+  
+  splash: boolean = true;
+  private citySubscription: Subscription = null;
+
+  constructor(private cityService: CityService){
+    setTimeout(() => {
+      this.citySubscription = this.cityService.city$.subscribe({
+        next: (value: City) => {
+          if (value === null){
+            this.splash = true;
+          }else{
+            this.splash = false;
+          }
+        }
+      });
+    });
+  }
+  
 }
