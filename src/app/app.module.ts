6d07cc15fba3aa60368a929b3c32782e7ec42be8
Aitor Espinosa <aitor@Mac-mini-de-Aitor.local>
2019-09-11 04:03:33 +0200

    Added Service Worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { WeatherComponent } from './weather/weather.component';
 import { ArtboardComponent } from './artboard/artboard.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,7 @@ import { ArtboardComponent } from './artboard/artboard.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
   ],
   providers: [],
   bootstrap: [AppComponent]
